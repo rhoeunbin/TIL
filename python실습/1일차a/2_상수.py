56e053cbbe2e9445726eb36cb1151dc08009d872
rhoeunbin <rhoeb21@gmail.com>
2022-08-18 23:49:34 +0900

    1일차

--- python실습/1일차a/2_상수.py
+++ python실습/1일차a/2_상수.py
@@ -2,5 +2,5 @@ a, b = input().split()  #map 굳이 쓸 필요 X
 
 a = a[::-1] #역순으로
 b = b[::-1]
-
+print(type(a))
 print(max(a,b))  # a와 b 중 큰 값
--- python실습/1일차a/3_OX.py
+++ python실습/1일차a/3_OX.py
@@ -1,16 +1,16 @@
 n = int(input())
 
 for i in range(n):
-    ox = list(map(str,input()))
+    ox = list(map(str,input())) #OX 값을 리스트로 받음
     # print(ox)
-    cnt = 0
-    result = 0
+    cnt = 0 #'O'의 개수
+    result = 0 # 점수 결과
 
     for j in ox:
-        if j == 'O':
-            cnt += 1 
-            result += cnt
-        else :
+        if j == 'O': # 만약 j가 O라면
+            cnt += 1 #'O'의 개수 추가
+            result += cnt # 결과에 cnt 값 더하기
+        else : #j가 O가 아닐 때
             cnt = 0
 
     print(result)
