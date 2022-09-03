0f0ffec402d9d95a1925d0fe5ff21d9d40d1f9c5
rhoeunbin <rhoeb21@gmail.com>
2022-09-03 23:44:58 +0900

    2일차 실습

--- python실습/2일차a/24_한수.py
+++ python실습/2일차a/24_한수.py
@@ -1,1 +1,11 @@
-N = int(input())
+N = int(input()) #입력 받기
+
+han = 0 #한수 지정
+for i in range(1, N+1) :
+    if i <= 99: #99까지는 한수이니깐 +1
+        han += 1 
+    else : #99 이후
+        a = list(map(int, str(i))) #99이후의 숫자 넣기
+        if a[0] - a[1] == a[1] - a[2] : # 백의 자리- 십의자리 = 십의 자리 - 일의 자리
+            han += 1 #수가 같다면 한수 +1
+print(han)
