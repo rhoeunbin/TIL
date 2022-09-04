5e033fdc72267296c337a310bc1906c358a3914b
rhoeunbin <rhoeb21@gmail.com>
2022-09-04 23:48:54 +0900

    3일차실습

--- python실습/3일차a/42_세로읽기.py
+++ python실습/3일차a/42_세로읽기.py
@@ -1,22 +1,12 @@
-# alpha = []
+board = [] # 칠판에 적힌 문자열을 받을 리스트
+len_list = [] # 줄마다 길이를 저장해놓을 변수
 
-# for i in range(5):
-#     alpha.append(input())
+for i in range(5): # 가로 5줄 
+    board.append(list(input())) # 문자열을 받고 list로 더해준다.
+    len_list.append(len(board[i])) # 입력받은 문자열의 길이도 같이 저장한다.
+max_len = max(len_list) # 제일 큰 문자열의 길이를 찾아놓는다.
 
-#     for j in range(len(alpha)):
-
-
-
-
-
-matrix = [] 
-mx = 0
-for i in range(5):
-    k = input()
-    matrix.append(k)
-    mx = max(mx, len(k))
-print(mx)
-
-    # a = input()
-    #   k = 15 - len(a)
-    #   a += k * " "
+for j in range(max_len): # 세로 우선 순회를 max_len 만큼 한다.
+    for i in range(5): # 5줄의 문자열을
+        if j < len(board[i]): # 해당 문자열의 길이 이하의 범위일때
+            print(board[i][j], end='') # 공백없이 이어서 출력한다.
