9c14e15cef8b7d94e6eaf75871e1fc80280b5b76
rhoeunbin <rhoeb21@gmail.com>
2022-09-26 23:54:39 +0900

    7일차 실습

--- python실습/7일차a/69_절댓값힙.py
+++ python실습/7일차a/69_절댓값힙.py
@@ -6,7 +6,7 @@ n =int(input())  #연산의 개수
 heap = []        #heap 생성
 
 for i in range(n):    #n만큼
-    x = int(input()) #x = [1, -1, 0, 0, 0, 1, 1, -1, -1, 2, -2, 0, 0]
+    x = int(input()) #x = [1, -1, 0, 0, 0, 1, 1, -1, -1, 2, -2, 0, 0,0,0,0,0]
     if x != 0:       
         heapq.heappush(heap, (abs(x), x))  # heappush 값을 넣을 때
     else :
